c263a2cb05fe0d1f0205ae2d4f20982403e0c1a4
Ilya Torbin <il.torbin@gmail.com>
2025-02-20 09:43:04 +0300

    add: __all__ to pystrector

--- pystrector/__init__.py
+++ pystrector/__init__.py
@@ -3,3 +3,7 @@ from .base_datatypes import DataTypeMeta, Void, Int, Func, Array, \
     Pointer, UnsignedInt, LongLong, UnsignedLongLong, Byte, Bool, \
     UnsignedByte, Short, UnsignedShort, Float, Double, DataType, \
     get_anonymous_var_name
+
+__all__ = [
+    "Binder",
+]
