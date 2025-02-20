8cd2c3422316c980c1beec4687b56d94546ff52e
Ilya Torbin <il.torbin@gmail.com>
2025-02-20 20:54:33 +0300

    test: add tests

--- tests/test_double_type.py
+++ tests/test_double_type.py
@@ -1,29 +1,30 @@
 import unittest
+from struct import pack
+
 from pystrector import Binder
 
 binder = Binder()
 
 
-class TestNumber(unittest.TestCase):
+class TestDouble(unittest.TestCase):
     def test_set_value_object(self):
-        x = 999999
-        y = 888888
+        x = 1111.11
+        y = 2222.22
         reflector_x = binder.bind(x)
         reflector_y = binder.bind(y)
-        reflector_x.long_value.ob_digit = reflector_y.long_value.ob_digit
+        reflector_x.ob_fval = reflector_y.ob_fval
         self.assertEqual(x, y)
 
     def test_set_pretty_value(self):
-        x = 999999
-        y = 888888
+        x = 3333.33
+        y = 4444.44
         reflector_x = binder.bind(x)
-        reflector_x.long_value.ob_digit[0].pretty_value = y
+        reflector_x.ob_fval.pretty_value = y
         self.assertEqual(x, y)
 
     def test_set_bytes_value(self):
-        x = 1023
-        y = 2000
+        x = 5555.55
+        y = 6666.66
         reflector_x = binder.bind(x)
-        reflector_x.long_value.ob_digit[0].bytes_value = \
-            bytearray(y.to_bytes(length=4, byteorder="little"))
+        reflector_x.ob_fval.bytes_value = bytearray(pack('d', y))
         self.assertEqual(x, y)
