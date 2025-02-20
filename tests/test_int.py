a3495b75c5438ac8aec042524fa29743b215df43
Ilya Torbin <il.torbin@gmail.com>
2025-02-20 16:02:47 +0300

    test: add tests to int

--- tests/test_int.py
+++ tests/test_int.py
@@ -7,7 +7,7 @@ binder = Binder()
 
 class TestInt(unittest.TestCase):
 
-    def test_caching_ints(self):
+    def test_caching(self):
         # specific test, works only for Python 3.12+ (PEP 683)
         for i in range(-5, 257):
             reflector = binder.bind(i)
@@ -16,28 +16,29 @@ class TestInt(unittest.TestCase):
                 4294967295,
             )
 
-    def test_int_refcnt(self):
+    def test_refcnt(self):
         for i in range(257, 1000):
             reflector = binder.bind(i)
-            self.assertEqual(reflector.ob_base.ob_refcnt.pretty_value,
-                             getrefcount(i) - 1)
+            self.assertEqual(
+                reflector.ob_base.ob_refcnt.pretty_value,
+                getrefcount(i) - 1
+            )
 
-    def test_int_type(self):
+    def test_type(self):
         first_type_address = binder.bind(1).ob_base.ob_type.pretty_value
         second_type_address = binder.bind(2).ob_base.ob_type.pretty_value
         self.assertEqual(first_type_address, second_type_address)
 
-    def test_int_long_value(self):
+    def test_long_value(self):
         obj = 5
         reflector = binder.bind(obj)
         self.assertEqual(reflector.long_value.lv_tag.pretty_value, 8)
         self.assertEqual(reflector.long_value.ob_digit[0].pretty_value, obj)
         self.assertEqual(reflector.long_value.ob_digit[1].pretty_value, 0)
 
-    def test_int_set_value(self):
+    def test_set_value_object(self):
         x = 999999
         y = 888888
-
         reflector_x = binder.bind(x)
         reflector_y = binder.bind(y)
         reflector_x.long_value.ob_digit = reflector_y.long_value.ob_digit
@@ -45,3 +46,21 @@ class TestInt(unittest.TestCase):
 
         reflector_x.long_value.ob_digit[0].pretty_value = 777777
         self.assertEqual(x, 777777)
+
+    def test_set_pretty_value(self):
+        x = 999999
+        y = 888888
+        reflector_x = binder.bind(x)
+        reflector_x.long_value.ob_digit[0].pretty_value = y
+        self.assertEqual(x, y)
+        reflector_x.long_value.ob_digit[0].pretty_value = 999999
+
+    def test_set_bytes_value(self):
+        x = 1023
+        y = 2000
+        reflector_x = binder.bind(x)
+        reflector_x.long_value.ob_digit[0].bytes_value = \
+            bytearray(y.to_bytes(length=4, byteorder="little"))
+        self.assertEqual(x, y)
+        reflector_x.long_value.ob_digit[0].bytes_value = \
+            bytearray((2000).to_bytes(length=4, byteorder="little"))
