9c3ed25791cd79f05ca7f7100c9724b539db0537
Ilya Torbin <il.torbin@gmail.com>
2025-02-20 10:06:53 +0300

    style: delete unused imports

--- pystrector/__init__.py
+++ pystrector/__init__.py
@@ -1,8 +1,4 @@
 from .binder import Binder
-from .base_datatypes import DataTypeMeta, Void, Int, Func, Array, \
-    Pointer, UnsignedInt, LongLong, UnsignedLongLong, Byte, Bool, \
-    UnsignedByte, Short, UnsignedShort, Float, Double, DataType, \
-    get_anonymous_var_name
 
 __all__ = [
     "Binder",
